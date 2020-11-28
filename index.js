dec96714222e6e8176869cbbc0db55e1bb00585a
John McLear <john@mclear.co.uk>
2020-11-28 10:40:01 +0000

    lint

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
+'use strict';
+
 const eejs = require('ep_etherpad-lite/node/eejs/');
-exports.eejsBlock_editbarMenuLeft = function (hook_name, args, cb) {
+
+exports.eejsBlock_editbarMenuLeft = (hookName, args, cb) => {
   args.content += eejs.require('ep_font_color/templates/editbarButtons.ejs');
   return cb();
 };
 
-exports.eejsBlock_dd_format = function (hook_name, args, cb) {
+exports.eejsBlock_dd_format = (hookName, args, cb) => {
   args.content += eejs.require('ep_font_color/templates/fileMenu.ejs');
   return cb();
 };
--- static/js/index.js
+++ static/js/index.js
@@ -1,14 +1,13 @@
-var _, $, jQuery;
+'use strict';
 
-var $ = require('ep_etherpad-lite/static/js/rjquery').$;
-var _ = require('ep_etherpad-lite/static/js/underscore');
-var cssFiles = ['ep_font_color/static/css/color.css'];
+const _ = require('ep_etherpad-lite/static/js/underscore');
+const cssFiles = ['ep_font_color/static/css/color.css'];
 
 // All our colors are block elements, so we just return them.
-var colors = ['black', 'red', 'green', 'blue', 'yellow', 'orange'];
+const colors = ['black', 'red', 'green', 'blue', 'yellow', 'orange'];
 
 // Bind the event handler to the toolbar buttons
-var postAceInit = function (hook, context) {
+const postAceInit = function (hook, context) {
   const hs = $('.color-selection');
   hs.on('change', function () {
     const value = $(this).val();
@@ -29,21 +28,20 @@ var postAceInit = function (hook, context) {
 };
 
 // Our colors attribute will result in a color:red... _yellow class
-function aceAttribsToClasses(hook, context) {
+const aceAttribsToClasses = (hook, context) => {
   if (context.key.indexOf('color:') !== -1) {
     const color = /(?:^| )color:([A-Za-z0-9]*)/.exec(context.key);
     return [`color:${color[1]}`];
   }
-  if (context.key == 'color') {
+  if (context.key === 'color') {
     return [`color:${context.value}`];
   }
-}
+};
 
 
 // Here we convert the class color:red into a tag
-exports.aceCreateDomLine = function (name, context) {
+exports.aceCreateDomLine = (name, context) => {
   const cls = context.cls;
-  const domline = context.domline;
   const colorsType = /(?:^| )color:([A-Za-z0-9]*)/.exec(cls);
 
   let tagIndex;
@@ -51,7 +49,6 @@ exports.aceCreateDomLine = function (name, context) {
 
 
   if (tagIndex !== undefined && tagIndex >= 0) {
-    const tag = colors[tagIndex];
     const modifier = {
       extraOpenTags: '',
       extraCloseTags: '',
@@ -66,32 +63,29 @@ exports.aceCreateDomLine = function (name, context) {
 // Find out which lines are selected and assign them the color attribute.
 // Passing a level >= 0 will set a colors on the selected lines, level < 0
 // will remove it
-function doInsertColors(level) {
+const doInsertColors = (level) => {
   const rep = this.rep;
   const documentAttributeManager = this.documentAttributeManager;
   if (!(rep.selStart && rep.selEnd) || (level >= 0 && colors[level] === undefined)) {
     return;
   }
 
-  let new_color = ['color', ''];
+  let newColor = ['color', ''];
   if (level >= 0) {
-    new_color = ['color', colors[level]];
+    newColor = ['color', colors[level]];
   }
 
-  documentAttributeManager.setAttributesOnRange(rep.selStart, rep.selEnd, [new_color]);
-}
+  documentAttributeManager.setAttributesOnRange(rep.selStart, rep.selEnd, [newColor]);
+};
 
 
 // Once ace is initialized, we set ace_doInsertColors and bind it to the context
-function aceInitialized(hook, context) {
+const aceInitialized = (hook, context) => {
   const editorInfo = context.editorInfo;
   editorInfo.ace_doInsertColors = _(doInsertColors).bind(context);
-}
+};
 
-function aceEditorCSS() {
-  return cssFiles;
-}
-
+const aceEditorCSS = () => cssFiles;
 
 // Export all hooks
 exports.aceInitialized = aceInitialized;
