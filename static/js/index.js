b07b6ecfaeb1645b5f622a8182475681ee9d7c5c
ilmar <ilmar.tyrk@gmail.com>
2021-01-06 16:08:55 +0200

    Update select value if selection has color

--- static/js/index.js
+++ static/js/index.js
@@ -94,9 +94,56 @@ const postToolbarInit = (hook, context) => {
   });
 };
 
+const aceEditEvent = (hook, call, cb) => {
+  const cs = call.callstack;
+  const attrManager = call.documentAttributeManager;
+  const rep = call.rep;
+  const allowedEvents = ['handleClick', 'handleKeyEvent'];
+  if (allowedEvents.indexOf(cs.type) === -1 && !(cs.docTextChanged)) {
+    return cb();
+  }
+
+  // If it's an initial setup event then do nothing..
+  if (cs.type === 'setBaseText' || cs.type === 'setup') return cb();
+  // It looks like we should check to see if this section has this attribute
+  setTimeout(() => { // avoid race condition..
+    const colorSelect = $('.color-selection, #color-selection');
+    colorSelect.val('dummy'); // reset value to the dummy value
+    colorSelect.niceSelect('update');
+    // Attribtes are never available on the first X caret position so we need to ignore that
+    if (rep.selStart[1] === 0) {
+      // Attributes are never on the first line
+      return;
+    }
+    // The line has an attribute set, this means it wont get hte correct X caret position
+    if (rep.selStart[1] === 1) {
+      if (rep.alltext[0] === '*') {
+        // Attributes are never on the "first" character of lines with attributes
+        return;
+      }
+    }
+    // the caret is in a new position.. Let's do some funky shit
+    const startAttribs = attrManager.getAttributesOnPosition(rep.selStart[0], rep.selStart[1]);
+    const endAttribs = attrManager.getAttributesOnPosition(rep.selEnd[0], rep.selEnd[1]);
+    const [startColor] = startAttribs.filter((item) => item[0] === 'color');
+    const [endColor] = endAttribs.filter((item) => item[0] === 'color');
+    if (!startColor && !endColor) return;
+    $.each(colors, (k, v) => {
+      if (startColor && startColor[1] === v && (!endColor || endColor[1] === v)) {
+        colorSelect.val(k);
+      } else if (!startColor && endColor[1] === v) {
+        colorSelect.val(k);
+      }
+    });
+    colorSelect.niceSelect('update');
+  }, 250);
+
+  return cb();
+};
 // Export all hooks
 exports.postToolbarInit = postToolbarInit;
 exports.aceInitialized = aceInitialized;
 exports.postAceInit = postAceInit;
 exports.aceAttribsToClasses = aceAttribsToClasses;
+exports.aceEditEvent = aceEditEvent;
 exports.aceEditorCSS = () => ['ep_font_color/static/css/color.css'];
